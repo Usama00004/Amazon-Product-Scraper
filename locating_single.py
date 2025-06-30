8d8b2d54ed345ef4afcf5a08ce997cb8fbea15a7
Usama Tahir <usamatahir00004@gmail.com>
2025-06-30 18:54:46 +0200

    Adding locating single file

--- locating_single.py
+++ locating_single.py
@@ -5,6 +5,14 @@ import time
 
 driver = webdriver.Chrome()
 query = "laptop"
-driver.get(f"https://www.amazon.in/s?k={query}&crid=2AUXV5X8ZBTR&sprefix=laptop%2Caps%2C1123&ref=nb_sb_noss_2")
+for i in range (1,20):
+    driver.get("https://www.amazon.in/s?k=laptops&crid=2AUXV5X8ZBTR&sprefix=laptop%2Caps%2C1123&ref=nb_sb_noss_2")
+    elems = driver.find_element(By.CLASS_NAME, "puis-card-container")
+    print(f"{len(elems)} Items Found !!")
+    for elem in elems:
+        print(elem.text)
+
 time.sleep(10)
 driver.close()
+
+
