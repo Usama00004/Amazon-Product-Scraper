cf1ca71135dc5e42696c958e711338c3112721b8
Usama Tahir <usamatahir00004@gmail.com>
2025-06-30 20:08:05 +0200

    Fetching Data

--- locating_single.py
+++ locating_single.py
@@ -6,7 +6,7 @@ import time
 driver = webdriver.Chrome()
 query = "laptop"
 for i in range (1,20):
-    driver.get("https://www.amazon.in/s?k=laptops&crid=2AUXV5X8ZBTR&sprefix=laptop%2Caps%2C1123&ref=nb_sb_noss_2")
+    driver.get("https://www.amazon.de/s?k=laptops&crid=2AUXV5X8ZBTR&sprefix=laptop%2Caps%2C1123&ref=nb_sb_noss_2")
     elems = driver.find_element(By.CLASS_NAME, "puis-card-container")
     print(f"{len(elems)} Items Found !!")
     for elem in elems:
